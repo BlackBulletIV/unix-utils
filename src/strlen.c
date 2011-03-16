9a912d7aaa5a71ba6813ac4df936b3c8f3b04dd4
Michael Ebens <michael@nova-fusion.com>
2011-03-16 21:07:22 +1100

    Changed to two space indentation and fixed some errors caused by plain C.

--- src/strlen.c
+++ src/strlen.c
@@ -4,32 +4,35 @@
 
 int main(int argc, char* argv[])
 {
-    int i = 0;
-    int len = 0;
+  int i = 0;
+  size_t len = 0;
+  int gets_len = 0;
+  char str[256];
 
-    if (argc == 1)
+  if (argc == 1)
+  {
+    while (fgets(str, 256, stdin))
     {
-        char str[256];
-        while (fgets(str, 256, stdin))
-        {
-            int gets_len = strlen(str);
-            len += gets_len;
-            if (str[gets_len - 1] == '\n') len--; // get rid of new lines
-        }
-        
-        if (!str)
-        {
-            puts("Error: You gave no arguments!");
-            return EXIT_FAILURE;
-        }
+      gets_len = strlen(str);
+      len += gets_len;
+      if (str[gets_len - 1] == '\n') len--; // get rid of new lines
     }
 
+    if (!str)
+    {
+      puts("Error: You gave no arguments!");
+      return EXIT_FAILURE;
+    }
+  }
+  else
+  {
     for (i = 1; i < argc; i++)
     {
-        len += strlen(argv[i]);
-        if (i > 1) len++; // for the space
+      len += strlen(argv[i]);
+      if (i > 1) len++; // for the space
     }
+  }
 
-    printf("%i\n", len);
-    return EXIT_SUCCESS;
+  printf("%i\n", (int) len);
+  return EXIT_SUCCESS;
 }
